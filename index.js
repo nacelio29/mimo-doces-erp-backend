7609a2c36b6e83f77865865e3d4ff37c65c965ab
nacelio29 <nacelio29@gmail.com>
2026-04-13 20:48:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Mimo Doces ERP rodando 🚀");
 });
 
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000 🚀");
+});
+
 app.get("/teste-banco", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM usuarios");
